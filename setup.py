09edcb2707c002afd1c41a159c238fef174fe2df
Tomáš Zítka <tozitka@gmail.com>
2021-06-19 18:44:17 +0200

    Explicitly use UTF-8 when loading readme.
    - this prevents encoding trouble on windows systems

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 version = lucent.__version__
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
